7737140653c48bc4deeed4317208e4948000c399
Inciverse <inciverse001@gmail.com>
2026-01-02 01:21:29 +0500

    Update ReceiveBox.tsx

--- components/ReceiveBox.tsx
+++ components/ReceiveBox.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { supabase } from "@/lib/supabaseClient";
+
 import { Eye, EyeOff, Download, Shield } from "lucide-react";
 
 export default function ReceiveBox() {
