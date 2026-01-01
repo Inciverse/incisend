e63823e6ebd31450f221dd2406392bcf45e2974a
Inciverse <inciverse001@gmail.com>
2026-01-01 22:27:36 +0500

    Update page.tsx

--- app/send/page.tsx
+++ app/send/page.tsx
@@ -2,8 +2,8 @@ import InciSend from "../../components/InciSend";
 
 export default function SendPage() {
   return (
-    <main className="min-h-screen bg-[#F8FAFC] py-12">
-      {/* Explicitly telling the component to start in 'send' mode */}
+    <main className="min-h-screen bg-[#FDFDFF] py-16 px-4">
+      {/* initialMode prop ensures it always opens Send tab */}
       <InciSend initialMode="send" />
     </main>
   );
