fa8ca37a1f9ba6a9b2ecaad6aa29a6e9652dd859
Inciverse <inciverse001@gmail.com>
2026-01-02 01:17:13 +0500

    Update InciSend.tsx

--- components/InciSend.tsx
+++ components/InciSend.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { supabase } from "@/lib/supabaseClient";
+
 import { Upload, Eye, EyeOff, Shield, Check } from "lucide-react";
 
 function generateCode() {
