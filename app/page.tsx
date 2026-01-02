77e87963f2693b10222c3d62940ec56e3280b097
Inciverse <inciverse001@gmail.com>
2026-01-02 16:32:12 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,13 @@ export default function Home() {
             <button className="primary">Start Upload</button>
           </Link>
 
-          <Link href="/receive">
+          <Link href="/pricing">
+        <button className="border border-[var(--primary)] text-[var(--primary)] px-6 py-2 rounded-lg hover:bg-[var(--primary-light)] transition">
+        View Pricing
+         </button>
+          </Link>
+          
+            <Link href="/receive">
             <button className="secondary">Have a Code ?</button>
           </Link>
         </div>
