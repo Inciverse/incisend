609f49771bae706460135e8b0fe21568d40d9889
Inciverse <inciverse001@gmail.com>
2026-01-03 00:22:29 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,8 +25,8 @@ export default function RootLayout({
               <Image
                  src="/logo.png"
                   alt="Incisend"
-               width={80}
-              height={16}
+               width={50}
+              height={20}
                priority
               />
                 </Link>
